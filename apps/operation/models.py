1dbc4f50138bd19339f66d185edcf1bcdd8f483f
wsh <wang.shihua@outlook.com>
2019-05-23 23:58:33 +0800

    add midels

--- apps/operation/models.py
+++ apps/operation/models.py
@@ -1,4 +1,4 @@
-
+# _*_ encoding:utf-8 _*_
 from datetime import datetime
 
 from django.db import models
@@ -34,7 +34,34 @@ class CourseComments(models.Model):
 class UserFavorite(models.Model):
     user = models.ForeignKey(UserProfile, verbose_name="用户")
     fav_id = models.IntegerField(default=0, verbose_name="数据id")
-    fav_type = models.IntegerField()
+    fav_type = models.IntegerField(choices=((1, "课程"),(2, "课程机构"),(3, "讲师")), default=1, verbose_name="收藏类型")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "用户收藏"
+        verbose_name_plural = verbose_name
+
+
+class UserMessage(models.Model):
+    user = models.IntegerField(default=0, verbose_name="接收用户")
+    message = models.CharField(max_length=500, verbose_name="消息内容")
+    has_read = models.BooleanField(default=False, verbose_name="是否已读")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "用户消息"
+        verbose_name_plural = verbose_name
+
+
+class UserCourse(models.Model):
+    user = models.ForeignKey(UserProfile, verbose_name="用户")
+    course = models.ForeignKey(Course, verbose_name="课程")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "用户课程"
+        verbose_name_plural = verbose_name
+
 
 
 
